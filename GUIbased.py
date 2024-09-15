f66bb714b74825b010966a46e68e57da9f967739
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2024-09-15 14:30:28 +0300

    Update GUIbased.py

--- GUIbased.py
+++ GUIbased.py
@@ -1,1 +1,21 @@
+import tkinter as tk
+from tkinter import ttk, messagebox, filedialog
+import re
+import pyperclip
+import webbrowser
+from youtubesearchpython import VideosSearch
+from tkinter.scrolledtext import ScrolledText
+from ttkthemes import ThemedTk
+import threading
 
+# Global variable to store video URLs
+video_urls = []
+
+# Function to extract video ID from YouTube URL
+def extract_video_id(url):
+    match = re.search(r'(v=|youtu\.be/)([^&]+)', url)
+    if match:
+        return match.group(2)
+    else:
+        show_message("Error", "Invalid YouTube URL", "error")
+        return None
